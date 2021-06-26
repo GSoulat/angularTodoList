9a629e9c75f6993f9da44336046db603d590e7c6
Gsoulat <gsoulat31@gmail.com>
2021-06-26 16:44:17 +0200

    3 eme commit
    mise en place d'un service

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -6,20 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-  todoOne: string = "Projet 1";
-  todoTwo: string = "Projet 2";
-  todoThree: string = "Projet 3";
-  todoFour: string = "Projet 4";
-  todoFive: string = "Projet 5";
-  isModif = false;
+ today: Date;
+ todos: any;
 
 
 
-  constructor() { }
+  constructor(private todoService : TodoService) { }
 
   ngOnInit(): void {
+    this.today = this.todoService.today;
+    this.todos = this.todoService.todos;
+
+  }
+  onChangeStatus(i:number) {
+    this.todoService.onChangeStatus(i);
   }
 
+  onChangeIsModif(i:number) {
+    this.todoService.onChangeIsModif(i);
 
+  }
 
 }
